ca23191bfbee27e6e4bf53e617b62af3be2c197f
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2023-10-20 00:10:06 +0100

    add -with-report to create html report file

--- qsub_job.sh
+++ qsub_job.sh
@@ -4,6 +4,7 @@
 #$ -l h_rt=01:00:00 ###HH:MM:SS
 #$ -l h_vmem=25G
 #$ -pe sharedmem 2 
+#$ -P roslin_macqueen_lab
 
 module load igmm/apps/TrimGalore/0.6.6
 module load igmm/apps/cutadapt/1.16
@@ -15,4 +16,4 @@ module load roslin/nextflow/22.10.7
 module load igmm/apps/bwa/0.7.16
 module load igmm/apps/samtools/1.2
 
-nextflow run workflow.nf
+nextflow run workflow.nf -with-report report.html
