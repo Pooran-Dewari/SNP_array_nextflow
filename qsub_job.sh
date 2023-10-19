f4d01673c154c2d3ff45e6417d0f0ea9c73f325a
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2023-10-19 18:32:00 +0100

    Create qsub_job.sh

---
+++ qsub_job.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#$ -V -cwd
+#$ -l h_rt=01:00:00 ###HH:MM:SS
+#$ -l h_vmem=25G
+#$ -pe sharedmem 2 
+
+module load igmm/apps/TrimGalore/0.6.6
+module load igmm/apps/cutadapt/1.16
+module load igmm/apps/FastQC/0.11.9
+module load roslin/nextflow/22.10.7
+
+module load roslin/nextflow/22.10.7
+
+module load igmm/apps/bwa/0.7.16
+module load igmm/apps/samtools/1.2
+
+nextflow run workflow.nf
